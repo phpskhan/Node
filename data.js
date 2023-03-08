4088b728e81f6f6bec9dd4274ed8bdb7061e4b5e
Khurram Sabih <122235803+phpskhan@users.noreply.github.com>
2023-03-09 00:49:41 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,10 @@
-module.exports=[
+const data=[
     {name:'Sabih Khan', email:'phpskhan@gmail.com'},
+    {name:'Samina Khan', email:'minakhan@gmail.com'},
     {name:'Dua Khan', email:'duakhan@gmail.com'},
     {name:'Azlaan Khan', email:'azikhan@gmail.com'},
     {name:'Fatima Khan', email:'fatkhan@gmail.com'},
     {name:'Abaan Khan', email:'abikhan@gmail.com'}
     ]
+
+module.exports=data;
